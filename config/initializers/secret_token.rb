4ea834e5385ca5ccfb8be0aa3cd3c9cd0e6a2dd0
Grassroots Oregon <grassrootsoregon420@gmail.com>
2016-03-05 01:10:33 -0800

    Created a recycle for Bernie Sanders webpage and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recycle::Application.config.secret_token = '634e5db105f593525917475bc86abb1e0de447566ac29ce573f3133c6268b339e1efaee8603139c73fe4ce9fb416657feb8737e4b307f6936f23aa6fab7d282c'
